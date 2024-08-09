8e3866fe055a9f76fbf9fa6eaa3d15a6b3b0fa39
Gleb Staheev <g.staheev@yandex.ru>
2024-08-09 13:29:50 +0300

    connecting to Eureka Server

--- src/main/java/com/example/mc_country/McCountryApplication.java
+++ src/main/java/com/example/mc_country/McCountryApplication.java
@@ -2,10 +2,12 @@ package com.example.mc_country;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class McCountryApplication {
 
 	public static void main(String[] args) {
