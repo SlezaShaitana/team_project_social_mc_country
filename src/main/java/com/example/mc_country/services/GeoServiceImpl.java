62e9f0011d03751694b79feb0d48df94251e1a15
Gleb Staheev <g.staheev@yandex.ru>
2024-08-28 11:05:21 +0300

    add Map

--- src/main/java/com/example/mc_country/services/GeoServiceImpl.java
+++ src/main/java/com/example/mc_country/services/GeoServiceImpl.java
@@ -35,6 +35,7 @@ public class GeoServiceImpl implements GeoService{
     private final GeoClient geoClient;
     private final RedisTemplate redisTemplate;
     private final String prefixKeyName = "Index_";
+    Map<String,IndexCountry> indexes;
 
 
     @Override
@@ -56,7 +57,7 @@ public class GeoServiceImpl implements GeoService{
                     " Error: " + e.getMessage());
         }
 
-        Map<String, IndexCountry> indexes = new HashMap<>();
+        indexes = new HashMap<>();
         List<CountryDto> countries = new  ForkJoinPool().invoke(
                 new GetterCountries(areas, indexes, geoClient));
 
@@ -83,12 +84,10 @@ public class GeoServiceImpl implements GeoService{
             List<CityDto> cities = (List<CityDto>) redisTemplate.boundListOps(countryId.toString()).leftPop();
             log.info("Выгрузка городов страны с id: {} из Redis завершилась успешно", countryId);
             return cities;
-        }else if (Boolean.TRUE.equals(redisTemplate.hasKey(prefixKeyName + countryId))){
-            IndexCountry index = (IndexCountry) redisTemplate.boundListOps(prefixKeyName + countryId).leftPop();
-            log.info("Индекс страны с id: {} получен", countryId);
-            redisTemplate.opsForList().leftPush(prefixKeyName + countryId,index);
-            redisTemplate.expire(prefixKeyName, 1, TimeUnit.DAYS);
-            List<CityDto> cities = GetterCities.getCities(UUID.fromString(countryId), index.getIndex(), geoClient);
+        }else if (indexes.containsKey(prefixKeyName + countryId)){
+//            IndexCountry index = (IndexCountry) redisTemplate.boundListOps(prefixKeyName + countryId).leftPop();
+//            log.info("Индекс страны с id: {} получен", countryId);
+            List<CityDto> cities = GetterCities.getCities(UUID.fromString(countryId), indexes.get(prefixKeyName + countryId).getIndex(), geoClient);
 
             if (!GetterCities.getError().isEmpty()){
                 String error = GetterCities.getError();
