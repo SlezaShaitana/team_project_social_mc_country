0d0f91dcb0a74de3668e30310cbbbf9f4a5530ff
Gleb Staheev <g.staheev@yandex.ru>
2024-08-28 18:36:48 +0300

    UUID

--- src/main/java/com/example/mc_country/utils/GetterCountries.java
+++ src/main/java/com/example/mc_country/utils/GetterCountries.java
@@ -96,7 +96,7 @@ public class GetterCountries extends RecursiveTask<List<CountryDto>> {
                     String.valueOf(UUID.randomUUID()),
                     true,
                     countryDataFromHhApi.getName(),
-                    countryId
+                    UUID.fromString(countryId)
             ));
         }
         if (countryDataFromHhApi.getParentId() != null && !countryDataFromHhApi.getAreas().isEmpty()){
